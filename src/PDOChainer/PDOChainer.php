8e4445bdc63066de30e010c437b4fac9d3f81f18
Michael Slyshkin <m.slyshkin@gmail.com>
2016-06-21 16:48:54 +0300

    PDOChainer

---
+++ src/PDOChainer/PDOChainer.php
@@ -0,0 +1,143 @@
+<?php
+
+/**
+ * @author Michael Slyshkin <m.slyshkin@gmail.com>
+ */
+
+namespace PDOChainer;
+
+use RsORM\Query\Engine\MySQL\Statement;
+use PDOChainer\Exception;
+
+class PDOChainer {
+    
+    const UTF8 = "utf8";
+    
+    /**
+     * @var \PDO
+     */
+    private $_dbh;
+    
+    /**
+     * @var string
+     */
+    private $_host = "127.0.0.1";
+    
+    /**
+     * @var int
+     */
+    private $_port = 3306;
+    
+    /**
+     * @var string
+     */
+    private $_dbname = null;
+    
+    /**
+     * @var string
+     */
+    private $_user = "root";
+    
+    /**
+     * @var string
+     */
+    private $_pass = "";
+    
+    /**
+     * @var string
+     */
+    private $_charset = self::UTF8;
+    
+    /**
+     * @var array
+     */
+    private $_options = [];
+    
+    /**
+     * @param string $host
+     * @param int $port
+     * @param string $user
+     * @param string $pass
+     * @param string $dbname
+     */
+    public function __construct($host = null, $port = null, $user = null, $pass = null, $dbname = null) {
+        $this->_host = $host === null ? $this->_host : $host;
+        $this->_port = $port === null ? $this->_port : $port;
+        $this->_user = $user === null ? $this->_user : $user;
+        $this->_pass = $pass === null ? $this->_pass : $pass;
+        $this->_dbname = $dbname;
+    }
+    
+    /**
+     * @param string $charset
+     */
+    public function charset($charset) {
+        $this->_charset = $charset;
+    }
+    
+    /**
+     * @param array $options
+     */
+    public function options(array $options) {
+        $this->_options = $options;
+    }
+    
+    /**
+     * @param Statement\AbstractStatement $statement
+     * @param int $type
+     * @param string $class
+     * @return array
+     */
+    public function fetch(Statement\AbstractStatement $statement, $type, $class = null) {
+        $sth = $this->dbh()->prepare($statement->prepare());
+        $sth->execute($statement->values());
+        switch ($type) {
+            case \PDO::FETCH_ASSOC:
+                return $sth->fetchAll($type);
+            case \PDO::FETCH_CLASS:
+                return $sth->fetchAll($type, $class);
+        }
+    }
+    
+    /**
+     * @param Statement\AbstractStatement $statement
+     * @return array
+     */
+    public function fetchAssoc(Statement\AbstractStatement $statement) {
+        return $this->fetch($statement, \PDO::FETCH_ASSOC);
+    }
+    
+    /**
+     * @param Statement\AbstractStatement $statement
+     * @param string $class
+     * @return array
+     */
+    public function fetchClass(Statement\AbstractStatement $statement, $class) {
+        return $this->fetch($statement, \PDO::FETCH_CLASS, $class);
+    }
+    
+    private function init() {
+        $dsn = "mysql:host={$this->_host};port={$this->_port};";
+        if ($this->_dbname !== null) {
+            $dsn .= "dbname={$this->_dbname};";
+        }
+        try {
+            $this->_dbh = new \PDO($dsn, $this->_user, $this->_pass, $this->_options);
+            $this->_dbh->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            $this->_dbh->exec("set names {$this->_charset}");
+        } catch (\PDOException $e) {
+            new Exception\DBConnectionFail("Database error: {$e->getMessage()}");
+        }
+    }
+    
+    /**
+     * @return \PDO
+     */
+    private function dbh() {
+        if ($this->_dbh === null) {
+            $this->init();
+        }
+        return $this->_dbh;
+    }
+    
+}
