4d5b631fbd732e8341e6c2bbc0158a6fb56398b2
Michael Slyshkin <m.slyshkin@gmail.com>
2016-07-19 14:16:27 +0300

    Fix builder delete test

--- tests/Query/Engine/MySQL/Builder/DeleteTest.php
+++ tests/Query/Engine/MySQL/Builder/DeleteTest.php
@@ -8,11 +8,22 @@ namespace RsORMTest\Query\Engine\MySQL\Builder;
 
 use RsORMTest;
 use RsORM\Query\Engine\MySQL\Builder;
+use RsORM\Query\Engine\MySQL\Flag;
 
 class DeleteTest extends RsORMTest\Base {
     
-    public function test() {
-        $query = new Builder\Delete();
+    public function testShort() {
+        $query = Builder::delete()
+                ->table("table");
+        $stmt = $query->build();
+        $this->assertSame("DELETE FROM `table`", $stmt->prepare());
+        $this->assertSame([], $stmt->values());
+    }
+    
+    public function testFull() {
+        $query = Builder::delete()
+                ->table("table")
+                ->flags([new Flag\HighPriority()]);
         $query->table("table");
         $stmt = $query->build();
         $this->assertSame("DELETE FROM `table`", $stmt->prepare());
