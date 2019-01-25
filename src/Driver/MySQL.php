c1f69d7ec87876a19fc97a90aca4c4101c10f30f
Vladimir Boliev <voff.web@gmail.com>
2019-01-25 14:32:42 +0300

    #87 disable ATTR_EMULATE_PREPARES pdo attribute

--- src/Driver/MySQL.php
+++ src/Driver/MySQL.php
@@ -11,49 +11,49 @@ use RsORM\Driver\Exception\DB\Connection;
 use RsORM\State;
 
 class MySQL {
-    
+
     const UTF8 = "utf8";
-    
+
     /**
      * @var \PDO
      */
     private $_dbh;
-    
+
     /**
      * @var string
      */
     private $_host = "127.0.0.1";
-    
+
     /**
      * @var int
      */
     private $_port = 3306;
-    
+
     /**
      * @var string
      */
     private $_dbname = null;
-    
+
     /**
      * @var string
      */
     private $_user = "root";
-    
+
     /**
      * @var string
      */
     private $_pass = "";
-    
+
     /**
      * @var string
      */
     private $_charset = self::UTF8;
-    
+
     /**
      * @var array
      */
     private $_options = [];
-    
+
     /**
      * @param string $host
      * @param int $port
@@ -68,21 +68,21 @@ class MySQL {
         $this->_pass = $pass === null ? $this->_pass : $pass;
         $this->_dbname = $dbname;
     }
-    
+
     /**
      * @param string $charset
      */
     public function setCharset($charset) {
         $this->_charset = $charset;
     }
-    
+
     /**
      * @param array $options
      */
     public function setOptions(array $options) {
         $this->_options = $options;
     }
-    
+
     /**
      * @param Statement\AbstractStatement $statement
      * @return array
@@ -92,7 +92,7 @@ class MySQL {
         $sth->setFetchMode(\PDO::FETCH_ASSOC);
         return $sth->fetch();
     }
-    
+
     /**
      * @param Statement\AbstractStatement $statement
      * @return array
@@ -102,7 +102,7 @@ class MySQL {
         $sth->setFetchMode(\PDO::FETCH_ASSOC);
         return $sth->fetchAll();
     }
-    
+
     /**
      * @param Statement\AbstractStatement $statement
      * @param string $class
@@ -115,7 +115,7 @@ class MySQL {
         $this->_flushObject($object);
         return $object;
     }
-    
+
     /**
      * @param Statement\AbstractStatement $statement
      * @param string $class
@@ -128,14 +128,14 @@ class MySQL {
         $this->_flushObjects($objects);
         return $objects;
     }
-    
+
     /**
      * @param Statement\AbstractStatement $statement
      */
     public function query(Statement\AbstractStatement $statement) {
         $this->_query($statement);
     }
-    
+
     /**
      * @param string $query
      * @return \PDOStatement
@@ -143,14 +143,14 @@ class MySQL {
     public function queryCustom($query) {
         return $this->dbh()->query($query);
     }
-    
+
     /**
      * @return string
      */
     public function getLastInsertId() {
         return $this->dbh()->lastInsertId();
     }
-    
+
     private function _init() {
         $dsn = "mysql:host={$this->_host};port={$this->_port};";
         if ($this->_dbname !== null) {
@@ -159,12 +159,13 @@ class MySQL {
         try {
             $this->_dbh = new \PDO($dsn, $this->_user, $this->_pass, $this->_options);
             $this->_dbh->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            $this->_dbh->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
             $this->_dbh->exec("set names {$this->_charset}");
         } catch (\PDOException $e) {
             throw new Connection\Fail("Database error: {$e->getMessage()}");
         }
     }
-    
+
     /**
      * @return \PDO
      */
@@ -174,7 +175,7 @@ class MySQL {
         }
         return $this->_dbh;
     }
-    
+
     /**
      * @param Statement\AbstractStatement $statement
      * @return \PDOStatement
@@ -190,7 +191,7 @@ class MySQL {
         }
         return $result;
     }
-    
+
     /**
      * @param mixed $object
      */
@@ -199,7 +200,7 @@ class MySQL {
             State\Engine::getInstance()->flush($object);
         }
     }
-    
+
     /**
      * @param mixed[] $objects
      */
@@ -208,5 +209,5 @@ class MySQL {
             $this->_flushObject($object);
         }
     }
-    
+
 }
